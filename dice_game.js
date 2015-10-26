db354198959ef5227b12621178c9f9cdacf5387c
stonehippo <george@cantina.co>
2015-10-26 14:11:27 -0400

    fix typos

--- dice_game.js
+++ dice_game.js
@@ -15,17 +15,17 @@ let R = require('ramda');
 // range: Number -> Number -> Number
 let numberInRange = R.curry((min, max, n) => (n * (max - min + 1)) + min);
 
-// integerInRange:: int -> int -> int
+// integerInRange:: Number -> Number -> int
 let integerInRange = R.curry(R.compose(Math.floor, numberInRange));
 
-// die:: int -> (_ -> int)
+// die:: Number -> (_ -> int)
 let die = sides => () => integerInRange(1, sides, Math.random());
 
 // dice:: (_ -> int) -> int -> [(_ -> int)]
-let dice = R.curry((f, n) => () => R.repeat(f,n));
+let dice = R.curry((f, n) => () => R.repeat(f, n));
 
 // roll:: [(_ -> int)] -> int
-let roll = dice => R.reduce((a,b) => a + b(), 0, dice;
+let roll = dice => R.reduce((a, b) => a + b(), 0, dice);
 
 module.exports = {
     die: die,
