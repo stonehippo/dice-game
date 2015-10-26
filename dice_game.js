9da7ade5058e6ec93af4b38912ace3565a6ce992
stonehippo <george@cantina.co>
2015-10-26 13:28:52 -0400

    Add functions
    Add functions for generating real numbers and integers in an inclusive range, creating a die, creating multiple dice, and rolling dice.
    
    Export the dice aspects as an NPM module.

--- dice_game.js
+++ dice_game.js
@@ -3,20 +3,32 @@
  *
  * A tiny functional library for dice-based game mechanics
  *
+ * Makes use of ramda.js and ES6.
+ *
  * Copyright (c) 2015. George White
  */
 
-let R = require('ramda');
-// randonInt:: Number -> Number -> Number
-let randomInt = R.curry(R.compose(n => n + a, Math.floor, n=> (n * (a - b)), Math.random))
-// die:: Number -> (_ -> Number)
-let die = n => function() {};
-// roll:: (_ -> Number) -> Number -> (_ -> Number)
-let roll = R.curry((f, n) => function() {});
- 
+'use strict';
 
+let R = require('ramda');
+
+// range: Number -> Number -> Number
+var numberInRange = R.curry((min, max, n) => (n * (max - min + 1)) + min);
+
+// integerInRange:: int -> int -> int
+var integerInRange = R.curry(R.compose(Math.floor, numberInRange));
+
+// die:: int -> (_ -> int)
+var die = sides => () => integerInRange(1, sides, Math.random());
+
+// multipleDie:: (_ -> int) -> int -> [(_ -> int)]
+var dice = R.curry((f, n) => () => R.repeat(f,n));
+
+// roll:: [(_ -> int)] -> int
+var roll = dice => R.reduce((a,b) => a + b(), 0, dice);
 
 module.exports = {
-  
-}
-}
+    die: die,
+    dice: dice,
+    roll: roll
+};
