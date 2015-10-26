be479111ecf82a72f7dc08307699ca7dc4f8f9ce
stonehippo <george@cantina.co>
2015-10-26 13:58:19 -0400

    Change var to let in function assigments
    Use let instead of var. Correct the type signature on the dice function.

--- dice_game.js
+++ dice_game.js
@@ -13,19 +13,19 @@
 let R = require('ramda');
 
 // range: Number -> Number -> Number
-var numberInRange = R.curry((min, max, n) => (n * (max - min + 1)) + min);
+let numberInRange = R.curry((min, max, n) => (n * (max - min + 1)) + min);
 
 // integerInRange:: int -> int -> int
-var integerInRange = R.curry(R.compose(Math.floor, numberInRange));
+let integerInRange = R.curry(R.compose(Math.floor, numberInRange));
 
 // die:: int -> (_ -> int)
-var die = sides => () => integerInRange(1, sides, Math.random());
+let die = sides => () => integerInRange(1, sides, Math.random());
 
-// multipleDie:: (_ -> int) -> int -> [(_ -> int)]
-var dice = R.curry((f, n) => () => R.repeat(f,n));
+// dice:: (_ -> int) -> int -> [(_ -> int)]
+let dice = R.curry((f, n) => () => R.repeat(f,n));
 
 // roll:: [(_ -> int)] -> int
-var roll = dice => R.reduce((a,b) => a + b(), 0, dice);
+let roll = dice => R.reduce((a,b) => a + b(), 0, dice;
 
 module.exports = {
     die: die,
