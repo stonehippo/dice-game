9f097e0586e51ec84a3f8d010e577386fa8638ef
stonehippo <george@cantina.co>
2015-10-27 15:59:59 -0400

    Clean up type signatures

--- dice_game.js
+++ dice_game.js
@@ -12,28 +12,28 @@
 
 let R = require('ramda');
 
-// range: Number -> Number -> Number
+// range :: Number -> Number -> Number
 let numberInRange = R.curry((min, max, n) => (n * (max - min + 1)) + min);
 
-// integerInRange:: Number -> Number -> int
+// integerInRange :: Number -> Number -> int
 let integerInRange = R.curry(R.compose(Math.floor, numberInRange));
 
-// die:: Number -> (_ -> int)
+// die :: Number -> (_ -> int)
 let die = sides => () => integerInRange(1, sides, Math.random());
 
-// dice:: (_ -> int) -> int -> [(_ -> int)]
+// dice :: (_ -> int) -> int -> [(_ -> int)]
 let dice = R.curry((f, n) => () => R.repeat(f, n));
 
-// roll:: [(_ -> int)] -> int
+// roll :: [(_ -> int)] -> int
 let roll = dice => R.reduce((a, b) => a + b(), 0, dice);
 
-// isRollBelow:: int -> int -> boolean
+// isRollBelow :: int -> int -> boolean
 let isRollBelow = R.curry((c, r) => r < c );
 
-// isRollAbove:: int -> int -> boolean
+// isRollAbove :: int -> int -> boolean
 let isRollAbove = R.curry((f, r) => r > f );
 
-// isRollBetween:: int -> int -> int -> boolean
+// isRollBetween :: int -> int -> int -> boolean
 let isRollBetween = R.curry((c, f, r) => isRollAbove(c, r) && isRollBelow(f, r));
 
 module.exports = {
